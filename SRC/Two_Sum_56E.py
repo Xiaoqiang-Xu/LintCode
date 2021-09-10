e6bed83512e1bba2c7500779409092587de9ab54
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-09-09 23:44:23 -0400

    Update Two_Sum_56E.py

--- SRC/Two_Sum_56E.py
+++ SRC/Two_Sum_56E.py
@@ -16,3 +16,37 @@ class Solution:
                 return result
             um[target - numbers[i]] = i
         return [0, 0]
+      
+      
+      
+      
+class Solution:
+  # using two pointers method
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1, index2] (index1 < index2)
+    """
+    def twoSum(self, numbers, target):
+        # pay attention to the reference pass and value pass
+        numbers1 = numbers[:]
+        numbers1.sort()
+        n = len(numbers)
+        i, j = 0, n - 1
+        while i < j:
+            if numbers1[i] + numbers1[j] == target:
+                pair = [numbers1[i], numbers1[j]]
+                break
+            elif numbers1[i] + numbers1[j] > target:
+                j -=1
+            else:
+                i += 1
+        result = []        
+        for i in range(n):
+            if numbers[i] == pair[0] or numbers[i] == pair[1]:
+                result.append(i)
+        result.sort()
+        return result
+
+      
+      
