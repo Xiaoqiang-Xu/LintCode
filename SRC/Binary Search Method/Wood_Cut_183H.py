fa9267ff0485ae0364bdc25d9b21c660df0135df
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-10-14 21:55:05 -0400

    Create Wood_Cut_183H.py

---
+++ SRC/Binary Search Method/Wood_Cut_183H.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param L: Given n pieces of wood with length L[i]
+    @param k: An integer
+    @return: The maximum length of the small pieces
+    """
+    def woodCut(self, L, k):
+        # write your code here
+        if not L:
+            return 0
+
+        start, end = 1, min(max(L), sum(L) // k)
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if self.get_count(L, mid) >= k:
+                start = mid
+            else:
+                end = mid
+
+        return end if self.get_count(L, end) >= k else start
+
+    def get_count(self, L, length):
+        return sum(l // length for l in L)
