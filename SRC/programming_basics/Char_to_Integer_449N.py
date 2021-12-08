9e31fe9f3ba040f7c59f923c019b91311044eb8e
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-12-07 19:42:03 -0500

    Create Char_to_Integer_449N.py

---
+++ SRC/programming_basics/Char_to_Integer_449N.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param character: a character
+    @return: An integer
+    """
+    def charToInteger(self, character):
+        # write your code here
+
+        return ord(character)
+
+      # ord() can be used to convert a character to an integer
