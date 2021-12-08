7e55b9cb98b060fdafc7b9c8f167993dac040411
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-12-07 20:49:04 -0500

    Update Sort_Integers_463N.py

--- SRC/programming_basics/Sort_Integers_463N.py
+++ SRC/programming_basics/Sort_Integers_463N.py
@@ -20,3 +20,33 @@ class Solution:
         return A
 
 # using the selection sort O(n^2)
+
+
+
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    def sortIntegers(self, A):
+        # write your code here
+
+        n = len(A)
+
+        if n <= 1:
+            return A
+        
+        for i in range(n):
+            indicator = 0
+            for j in range(0, n - i - 1):
+                if A[j] > A[j + 1]:
+                    A[j], A[j + 1] = A[j + 1], A[j]
+                    indicator = 1
+            if indicator == 0:
+                return A
+
+        return A
+    # using the bubble sort with some optimization (after ith pass, the last i element are already sorted. We 
+    # can also stop the algorithm when we detect that no swap is happening)
+
+       
