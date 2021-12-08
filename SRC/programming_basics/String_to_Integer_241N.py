26b74daa97354470b957ffc72358f57f00143697
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-12-07 19:32:26 -0500

    Create String_to_Integer_241N.py

---
+++ SRC/programming_basics/String_to_Integer_241N.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param target: A string
+    @return: An integer
+    """
+    def stringToInteger(self, target):
+        # write your code here
+
+        return int(target)
+
+# int() can convert to int type.
