e265f98aa99a9746a339f9d7afa5e6d8b6c2cb50
Xiaoqiang Xu <55418642+Xiaoqiang-Xu@users.noreply.github.com>
2021-12-07 19:27:15 -0500

    Create Lowercase_to_UpperII_146N.py

---
+++ SRC/programming_basics/Lowercase_to_UpperII_146N.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param letters: A string
+    @return: A string
+    """
+    def lowercaseToUppercase2(self, letters: str) -> str:
+        # write your code here
+        result = letters.upper()
+        return result
+
+        # can use the method for string: string.upper()
